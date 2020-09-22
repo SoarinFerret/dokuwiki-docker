5618459baa9dde6ad72f2b9663ad0b6fb70e3320
Soarinferret <thecodye@kanto.cloud>
2020-09-21 20:29:20 -0500

    update docker container

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,19 @@
-FROM bitnami/dokuwiki:latest
+ARG DOKUWIKI_VERSION=20200729
 
+FROM bitnami/dokuwiki:${DOKUWIKI_VERSION}
+
+USER 0
 COPY ./gitbacked_setup.sh /
 
 RUN install_packages git openssh-client && \
+    #rm -r /var/lib/apt/lists /var/cache/apt/archives && \
     chmod +x /gitbacked_setup.sh && \
-    sed -i "/info \"Starting dokuwiki... \"/a \ \ /gitbacked_setup.sh" /app-entrypoint.sh
+    #sed -i "/info \"\*\* Starting DokuWiki setup \*\*\"/a \ \ \ \ /gitbacked_setup.sh" /opt/bitnami/scripts/dokuwiki/entrypoint.sh
+    # /opt/bitnami/scripts/dokuwiki/setup.sh
+    sed -i "/\/opt\/bitnami\/scripts\/dokuwiki\/setup.sh/a \ \ \ \ /gitbacked_setup.sh" /opt/bitnami/scripts/dokuwiki/entrypoint.sh
+RUN useradd -s /bin/sh -u 1001 -G 0 bitnami && mkdir -p /home/bitnami/.ssh && chmod 770 /home/bitnami/.ssh && chown 1001:0 /home/bitnami/.ssh -R
+
+USER 1001
 
 ENV SSH_RSA_KEY="" \
     GIT_URL="" \
--- gitbacked_setup.sh
+++ gitbacked_setup.sh
@@ -1,33 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+
+# Load bitnami libraries
+. /opt/bitnami/scripts/libbitnami.sh
+. /opt/bitnami/scripts/liblog.sh
+. /opt/bitnami/scripts/libwebserver.sh
 
 if [ -z "$SSH_RSA_KEY" -o -z "$GIT_REPO" -o -z "$GIT_FINGERPRINT" ]; then
-  echo "DOKU_GITBACKED: Variables SSH_RSA_KEY, GIT_REPO, GIT_FINGERPRINT must be set for gitbacked to funciton"; exit;
+  info "DOKU_GITBACKED: Variables SSH_RSA_KEY, GIT_REPO, GIT_FINGERPRINT must be set for gitbacked to funciton"; exit;
 fi
 
 # Get DAEMON user homedir
-homedir=$( getent passwd "daemon" | cut -d: -f6 )
-
+homedir=$( getent passwd "bitnami" | cut -d: -f6 )
+userid=$( id -u )
 # Setup SSH for DAEMON user
-mkdir $homedir/.ssh
 echo $GIT_FINGERPRINT | base64 -d > $homedir/.ssh/known_hosts
 echo $SSH_RSA_KEY | base64 -d > $homedir/.ssh/id_rsa 
 
 # Permissions
-chown daemon:root $homedir/.ssh -R
+chown $userid:0 $homedir/.ssh -R
 chmod 700 $homedir/.ssh -R
 chmod 600 $homedir/.ssh/id_rsa
 
 
 if [ ! -d "/bitnami/dokuwiki/data/docs" ]; then
-    echo "DOKU_GITBACKED: Cloning GIT_REPO to /bitnami/dokuwiki/data/docs"
-    sudo -u daemon git clone $GIT_REPO /bitnami/dokuwiki/data/docs
+    info "DOKU_GITBACKED: Cloning GIT_REPO to /bitnami/dokuwiki/data/docs"
+    git clone $GIT_REPO /bitnami/dokuwiki/data/docs
 
-    echo "DOKU_GITBACKED: Setting GIT CONFIG local properties"
+    info "DOKU_GITBACKED: Setting GIT CONFIG local properties"
     cd /bitnami/dokuwiki/data/docs
-    sudo -u daemon git config --local user.name dokuwiki
-    sudo -u daemon git config --local user.email $DOKUWIKI_EMAIL
+    git config --local user.name dokuwiki
+    git config --local user.email $DOKUWIKI_EMAIL
 else
-    echo "DOKU_GITBACKED: /bitnami/dokuwiki/data/docs already exists"
+    info "DOKU_GITBACKED: /bitnami/dokuwiki/data/docs already exists"
 fi
 
-echo "DOKU_GITBACKED: note - you may need to update DOKUWIKI global conf to use the directories within /bitnami/dokuwiki/data/docs"
+info "DOKU_GITBACKED: note - you may need to update DOKUWIKI global conf to use the directories within /bitnami/dokuwiki/data/docs"
